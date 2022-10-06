1a54beffcf5a2dee6edd523240c032e2774c7bdb
sooyun <so__oyeonii@naver.com>
2022-10-06 20:37:57 +0900

    테스트 문제 재도전!

--- src/App.js
+++ src/App.js
@@ -1,50 +1,107 @@
 import React, {useState} from "react";
 import "./App.css"
 
-export default function App () {
-  
+export default function App() {
   const [todo, setTodo] = useState("")
-  const changeInput = (event) => {
+  const inputBox = (event) => {
     setTodo(event.target.value)
   }
 
-  console.log(todo)
-  
-  const submitInput = (event) => {
-    event.preventDefault()    
-    if(todo.trim() === "") return
-    setTodo(todo)
+  const [todos, setTodos] = useState([])
+  const enterInput = (event) => {
+    event.preventDefault()
+    if (todo.trim() === "") return 
+      setTodos([...todos, todo])
+      setTodo("")    
   }
-  
 
- return (
-    <div className="total">
-      <div className="adding-box">
-        <form onSubmit={submitInput}>
-          <input          
-          type="text"
-          value={todo}          
-          onChange={changeInput}
-          />
-          <button type="submit" className="adding-but">추가하기</button>
-        </form>
-          
+  return (
+    <div>
+
+      <div className="title">
+        <h1>MY TODO LIST📜</h1>
       </div>
 
-      <h1 className="todoname">Todo List</h1>
-      <br></br>
-
-      {/* {todo.map((todos) => {return (
-        <div className="todobox">
-          {todos}         
-        </div>
-      )})} */}
-
-        <div className="todobox">
-          {todo}         
-        </div>
+      <form onSubmit={enterInput} className="input-box">
+        <div className="enterword">
+        <input
+        type="text"
+        value={todo}
+        placeholder="write your todo list!"
+        onChange={inputBox}
+      />    
+      <button className="save-but"> SAVE </button>
+      </div>
+      <div className="box">
+        {todos.map((x) => {
+          return (
+            <div className="save-box">
+              <span>{x}</span>
+            </div>
+          )
+        })
+      }
       
+      </div>
+      </form>
 
     </div>
- )
+  )
 }
+
+
+
+
+
+
+
+// import React, {useState} from "react";
+// import "./App.css"
+
+// export default function App () {
+  
+//   const [todo, setTodo] = useState("")
+//   const changeInput = (event) => {
+//     setTodo(event.target.value)
+//   }
+
+//   // console.log(todo)
+  
+//   const [todos, setTodos] = useState([])
+//   const submitInput = (event) => {
+//     event.preventDefault()    
+//     if(todo.trim() === "") return
+    
+//     setTodos([...todos, todo])
+//     setTodo("")
+//   }
+  
+
+
+//  return (
+//     <div className="total">
+//       <div className="adding-box">
+//         <form onSubmit={submitInput}>
+//           <input          
+//           type="text"
+//           value={todo}          
+//           onChange={changeInput}
+//           />
+//           <button type="submit" className="adding-but">추가하기</button>
+//         </form>
+          
+//       </div>
+
+//       <h1 className="todoname">Todo List</h1>
+//       <br></br>
+
+//       {todos.map((x) => {return (
+//         <div className="todobox">
+//           {x}         
+//         </div>
+//       )})}
+      
+
+//     </div>
+//  )
+// }
